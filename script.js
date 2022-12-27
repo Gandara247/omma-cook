f84c2e6cb47fca7559474fb7376aa691633953ae
Gandara247 <120376565+Gandara247@users.noreply.github.com>
2022-12-27 11:23:57 -0300

    Foi ativado a funcion do js para os slides top

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let count = 1;
-document.getElementById("radio1").Checked = true;
+document.getElementById("radio1").checked = true;
 
 setInterval( function(){
 nextImage();
@@ -11,5 +11,5 @@ function nextImage(){
        count = 1;
     }
 
-    document.getElementById("radio"+count).Checked = true;
+    document.getElementById("radio"+count).checked = true;
 }
